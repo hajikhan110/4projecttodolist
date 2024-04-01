c172baccb5114b6a1c16954e1f1bee3325bac6fb
hajikhan <hajikhan110@yahoo.com>
2024-04-01 15:09:33 +0500

    msg

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let todo1 = [];
 //let todo: string[] = [];
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer"
 let todo1: any = [];
 //let todo: string[] = [];
