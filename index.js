ad69e75c0014ade3852bc902978fbc4a9e2034e1
hajikhan <hajikhan110@yahoo.com>
2024-04-03 11:22:38 +0500

    msg

--- index.js
+++ index.js
@@ -2,25 +2,33 @@
 import inquirer from "inquirer";
 let todo1 = [];
 //let todo: string[] = [];
-let condition = true;
-while (condition) {
+let condition = "ok";
+while (condition !== "n") {
     let addtaskans = await inquirer.prompt([
         {
             name: "todo",
             type: "input",
-            message: "Sir what item do u want to Add please"
-        },
-        {
-            name: "addmoreitem",
-            type: "confirm",
-            message: "Sir want to continue feed more  todo item? y/n",
-            default: "false"
+            message: "Sir what item do u want to Add please if not type n"
         }
+        // ,
+        // {
+        //     name: "addmoreitem",
+        // type: "confirm",
+        // message: "Sir want to continue feed more  todo item? y/n",
+        // default: "false"
+        // }
     ]);
+    if (addtaskans.todo == "n") {
+        console.log("ok bye Sir");
+        console.log("so for todo list Sir");
+        console.log(todo1);
+        break;
+    }
     todo1.push([addtaskans.todo]);
     console.log("so for todo list Sir");
     console.log(todo1);
     condition = addtaskans.addmoreitem;
+    condition = addtaskans.todo;
     //console.log(addtaskans.todo);
     //if(ans.admoreitem!=='Y') continue;
     //console.log(ans);
--- index.ts
+++ index.ts
@@ -2,29 +2,38 @@
 import inquirer from "inquirer"
 let todo1: any = [];
 //let todo: string[] = [];
-let condition: boolean = true;
-while(condition){
+let condition: string = "ok";
+while(condition !== "n"){
 
 let addtaskans: any = await inquirer.prompt([
     {
         name: "todo",
 type: "input",
-message: "Sir what item do u want to Add please"
+message: "Sir what item do u want to Add please if not type n"
 
-},
-{
-    name: "addmoreitem",
-type: "confirm",
-message: "Sir want to continue feed more  todo item? y/n",
-
-default: "false"
 }
+// ,
+// {
+//     name: "addmoreitem",
+// type: "confirm",
+// message: "Sir want to continue feed more  todo item? y/n",
+
+// default: "false"
+// }
 
 ]);
+if(addtaskans.todo=="n")
+{
+    console.log("ok bye Sir");
+    console.log("so for todo list Sir");
+console.log(todo1);
+    break;
+}
 todo1.push([addtaskans.todo]);
 console.log("so for todo list Sir");
 console.log(todo1);
 condition = addtaskans.addmoreitem;
+condition = addtaskans.todo;
 //console.log(addtaskans.todo);
 //if(ans.admoreitem!=='Y') continue;
 //console.log(ans);
